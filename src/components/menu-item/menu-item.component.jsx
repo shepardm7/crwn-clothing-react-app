4e792dcf978d513e31831ba32982ca891f4483b8
Sateek Roy <sateeksuper@gmail.com>
2019-11-19 23:45:24 -0500

    updated menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div className="background-image" style={{ backgroundImage: `url(${imageUrl}` }} />
-    <div className="content">
-      <span className="title">{title.toUpperCase()}</span>
-      <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  console.log(history);
+  console.log(match);
+  return (
+    <div className={`${size || ''} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl}` }} />
+      <div className="content">
+        <span className="title">{title.toUpperCase()}</span>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 MenuItem.propType = {
   title: PropTypes.string.isRequired,
@@ -19,4 +24,4 @@ MenuItem.propType = {
   size: PropTypes.string
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
