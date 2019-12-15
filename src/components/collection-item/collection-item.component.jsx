bf81c8fe4dee89360b859c80bb1c23dc612dd7b9
Sateek Roy <sateeksuper@gmail.com>
2019-12-15 15:14:49 -0500

    Updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url('${imageUrl}')`
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
-  </div>
+// redux helpers
+import { addItem } from '../../redux/cart/cart.actions';
+
+const CollectionItem = ({ item, addItem }) => (
+	<div className="collection-item">
+		<div
+			className="image"
+			style={{
+				backgroundImage: `url('${item.imageUrl}')`
+			}}
+		/>
+		<div className="collection-footer">
+			<span className="name">{item.name}</span>
+			<span className="price">{item.price}</span>
+		</div>
+		<CustomButton inverted onClick={() => addItem(item)}>
+			ADD TO CART
+		</CustomButton>
+	</div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+	addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+	null,
+	mapDispatchToProps
+)(CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
-  return (
-    // eslint-disable-next-line react/button-has-type
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-      {children}
-    </button>
-  );
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
+	const getClass = () => {
+		let btnClass = 'custom-button';
+		if (isGoogleSignIn) btnClass += ' google-sign-in';
+		if (inverted) btnClass += ' inverted';
+		return btnClass;
+	};
+	return (
+		// eslint-disable-next-line react/button-has-type
+		<button className={getClass()} {...otherProps}>
+			{children}
+		</button>
+	);
 };
 
 export default CustomButton;
