1a35e49560e26b23d55130cc673dc7d3f79c0e59
Sateek Roy <sateeksuper@gmail.com>
2019-12-16 17:10:34 -0500

    Removed redux-logger from production and deleted console.log in menu-item.component.jsx

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,8 +5,6 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
-  console.log(history);
-  console.log(match);
   return (
     <div className={`${size || ''} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div className="background-image" style={{ backgroundImage: `url(${imageUrl}` }} />
